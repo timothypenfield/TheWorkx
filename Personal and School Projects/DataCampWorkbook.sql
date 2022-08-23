def320bba67fed51af0a91e5e8bababd6e748d97
timothypenfield <111655955+timothypenfield@users.noreply.github.com>
2022-08-22 22:11:34 -0500

    8.22.22 datacamp work

--- Personal and School Projects/DataCampWorkbook.sql
+++ Personal and School Projects/DataCampWorkbook.sql
@@ -1,3 +1,4 @@
+
 SELECT country_code, size,
   CASE WHEN size > 50000000
             THEN 'large'
@@ -40,3 +41,29 @@ FROM cities AS c1
     ON c1.country_code = c2.code
 -- Order by descending country code
 ORDER BY c2.code DESC;
+
+/*
+Select country name AS country, the country's local name,
+the language name AS language, and
+the percent of the language spoken in the country
+*/
+Select c.name AS country, local_name, l.name AS language, percent
+-- From left table (alias as c)
+FROM countries AS c
+  -- Join to right table (alias as l)
+  left JOIN languages AS l
+    -- Match on fields
+    ON c.code = l.code
+-- Order by descending country
+ORDER BY country DESC;
+
+-- Select name, region, and gdp_percapita
+SELECT c.name, c.region, e.gdp_percapita
+-- From countries (alias as c)
+FROM countries AS c
+  -- Left join with economies (alias as e)
+  LEFT JOIN economies AS e
+    -- Match on code fields
+    ON c.code = e.code
+-- Focus on 2010
+WHERE e.year = 2010;
